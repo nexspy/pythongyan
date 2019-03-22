ff6e6637fb315213dd2acc94a3e8982a22c5768e
nexspy <nirmalmaswa@live.com>
2019-03-22 11:47:12 +0545

    added strings

--- test.py
+++ test.py
@@ -5,13 +5,47 @@ Very easy language
 msg = "hello world"
 print(msg)
 
+
+"""
+Numbers
+"""
 x = 5
+x = int(3.4) #converts to 3
 y = 2
 
 #11 must be bigger than 2 to print this :D
 if (x>y):
     print ("x is bigger than y")
 
+
+"""
+Strings
+"""
 fname = "Nirmal"
 lname = "Limbu"
+print(fname + " " + lname)
+#convert number to string
+num = str(2) #gives '2'
+print(type(num))
+#getting substring 
+a = "Hello, World!"
+print(a[1])
+print(a[0:5])
+#remove whitespace from start
+a = " Hello World"
+print(a.strip())
+#getting length
+print(len(a))
+"""
+Other functions
+.lower()
+.upper()
+.replace("H", "J")
+.split(",") -> this is like explode
+"""
+
+#getting input from command line
+# x = input()
+# print("hello", x)
+
 
