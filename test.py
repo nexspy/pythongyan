f4f00cddc40ba9afc6db283814e0649c85bae700
nexspy <nirmalmaswa@live.com>
2019-03-22 11:37:07 +0545

    second commit

---
+++ test.py
@@ -0,0 +1,17 @@
+"""
+Just a beginning of python world
+Very easy language
+"""
+msg = "hello world"
+print(msg)
+
+x = 5
+y = 2
+
+#11 must be bigger than 2 to print this :D
+if (x>y):
+    print ("x is bigger than y")
+
+fname = "Nirmal"
+lname = "Limbu"
+
